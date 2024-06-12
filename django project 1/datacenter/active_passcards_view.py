7ce4dc296ed2d32b72dc62e761a001b3f6d611cc
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-12 15:21:48 +0300

    Update active_passcards_view.py

--- django project 1/datacenter/active_passcards_view.py
+++ django project 1/datacenter/active_passcards_view.py
@@ -7,6 +7,6 @@ def active_passcards_view(request):
         
     all_passcards = Passcard.objects.filter(is_active = True)
     context = {
-        'active_passcards': all_passcards,  # люди с активными пропусками
+        'active_passcards': all_passcards,  
     }
     return render(request, 'active_passcards.html', context)
