8b7fe7139367122674e7a2b549973b0ef127328a
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-16 22:52:29 +0300

    Update storage_information_view.py

--- django project 1/datacenter/storage_information_view.py
+++ django project 1/datacenter/storage_information_view.py
@@ -1,22 +1,22 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit,is_visits_long
+from datacenter.models import Visit, is_visits_long
 from django.shortcuts import render
 from django.utils.timezone import localtime, timedelta
 from datetime import datetime
 
 def storage_information_view(request):
     non_closed_visit = Visit.objects.filter(leaved_at=None)
-    visit_info = []
+    person_visits = []
     for visit in non_closed_visit:
-        enter = visit.entered_at
-        timespend = localtime() - visit.entered_at
-        visit_info.append({
+        enter_in_storage = visit.entered_at
+        time_spend_in_storage = localtime() - visit.entered_at
+        person_visits.append({
             'who_entered': visit.passcard.owner_name,
-            'entered_at': enter,
-            "duration": timespend,
+            'entered_at': enter_in_storage,
+            'duration': time_spend_in_storage,
             'is_strange': is_visits_long(visit, minutes=60)
         })
     context = {
-        'non_closed_visits': visit_info,  
+        'non_closed_visits': person_visits,  
     }
     return render(request, 'storage_information.html', context)
