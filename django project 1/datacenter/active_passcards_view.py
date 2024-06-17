72e00aa801b3da12aba3e18b893c652f9917aec4
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-17 12:19:19 +0300

    Update active_passcards_view.py

--- django project 1/datacenter/active_passcards_view.py
+++ django project 1/datacenter/active_passcards_view.py
@@ -3,10 +3,8 @@ from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime, timedelta
 
+
 def active_passcards_view(request):
-        
-    active_passcards = Passcard.objects.filter(is_active = True)
-    context = {
-        'active_passcards': acctive_passcards,  
-    }
+    active_passcards = Passcard.objects.filter(is_active=True)
+    context = {'active_passcards': acctive_passcards, }
     return render(request, 'active_passcards.html', context)
